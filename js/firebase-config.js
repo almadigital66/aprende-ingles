7512113136a071402d51a866bed83e41cce51678
almadigital66 <rubencondechima@gmail.com>
2026-07-16 20:38:55 -0400

    Create js/firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Configuración de tu proyecto Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCS7cYK54l5LLKxC4p4ifzADmfD1L8D9cc",
+  authDomain: "aprende-ingles-b187e.firebaseapp.com",
+  projectId: "aprende-ingles-b187e",
+  storageBucket: "aprende-ingles-b187e.firebasestorage.app",
+  messagingSenderId: "753107246350",
+  appId: "1:753107246350:web:39f819009911c81e400b5f"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
